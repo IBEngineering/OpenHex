fd002b283a6b32310bc8c124f9c5524ff9d97b9f
MisterCavespider <cavespider12@gmail.com>
2017-04-24 12:49:00 +0200

    Updated run.py

--- run/run.py
+++ run/run.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python
-
-import sys
 from os import *
 
-DEBUG = False
-
-def printInfo(info):
-    if DEBUG:
-        print('[Run.py] %s' % info)
-
-def execJava(path):
-    system('java -jar %s' % path)
+def doGradleThings():
+    """
+    This doesn't check if it's in the right directory.
+    It simply assumes it can call ./gradlew.
+    It also won work on windows.
+    """
+    system("gradle build ; ./gradlew run")
 
 if __name__ == '__main__':
-    printInfo("Running with args: %s" % sys.argv)
-    printInfo("Argument count: %d" % len(sys.argv))
-    if len(sys.argv) == 1:
-        if sys.argv[0] == 'help':
-            printInfo("Use commands like this:")
-            printInfo("run.py <ignored> <file>")
-    elif len(sys.argv) == 2:
-        printInfo("Running java file %s ..." % sys.argv[1])
-        execJava(sys.argv[1])
-        printInfo("Done!")
+    doGradleThings();
